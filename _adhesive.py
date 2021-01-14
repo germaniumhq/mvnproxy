6f65ccaf148c4187ccbfbf7dd5d0bfa86ae71ec5
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2021-01-14 15:30:24 +0100

    Use the right repo location

--- _adhesive.py
+++ _adhesive.py
@@ -3,7 +3,7 @@ import germanium_py_exe  # type: ignore
 
 germanium_py_exe.pipeline(
     {
-        "repo": "git@github.com:bmustiata/mvnproxy.git",
+        "repo": "git@github.com:germaniumhq/mvnproxy.git",
         "binaries": {
             "name": "Python 3.8 on Linux x64",
             "platform": "python:3.8",
